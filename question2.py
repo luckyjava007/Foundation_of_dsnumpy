bb7fbcc08389639ff27064875d2cafccd0c8c40a
luckyjava007 <134775053+luckyjava007@users.noreply.github.com>
2023-09-02 19:15:26 +0530

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,11 +1,6 @@
 import numpy as np
-
-# Create a sample numpy array
 arr = np.array([1, 2, 3, 4, 5])
-
-# Check if none of the elements are zero
 none_are_zero = np.all(arr != 0)
-
 if none_are_zero:
     print("None of the elements are zero.")
 else:
